6c9ce32f8c4de17b93726ee112708fb8b3ceb13c
handymenny <handymenny@outlook.com>
2023-09-25 10:59:39 +0200

    fix tests for jdk 12-17

--- library/src/test/java/cz/mroczis/netmonster/core/SdkTest.kt
+++ library/src/test/java/cz/mroczis/netmonster/core/SdkTest.kt
@@ -3,6 +3,7 @@ package cz.mroczis.netmonster.core
 import android.os.Build
 import io.kotlintest.specs.FreeSpec
 import java.lang.reflect.Field
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 
 abstract class SdkTest(
@@ -21,10 +22,32 @@ abstract class SdkTest(
     fun setFinalStatic(field: Field, newValue: Any) {
         field.isAccessible = true
 
-        val modifiersField = Field::class.java.getDeclaredField("modifiers")
+        val modifiersField = getModifiersField()
         modifiersField.isAccessible = true
         modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
 
         field.set(null, newValue)
+    }
+
+    @Throws(Exception::class)
+    private fun getModifiersField(): Field {
+        return try {
+            Field::class.java.getDeclaredField("modifiers")
+        } catch (e: NoSuchFieldException) {
+            try {
+                val getDeclaredFields0: Method =
+                    Class::class.java.getDeclaredMethod("getDeclaredFields0", Boolean::class.javaPrimitiveType)
+                getDeclaredFields0.isAccessible = true
+                val fields = getDeclaredFields0.invoke(Field::class.java, false) as Array<Field>
+                for (field in fields) {
+                    if ("modifiers" == field.name) {
+                        return field
+                    }
+                }
+            } catch (ex: ReflectiveOperationException) {
+                e.addSuppressed(ex)
+            }
+            throw e
+        }
     }
 }
